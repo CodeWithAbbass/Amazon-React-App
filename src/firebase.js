d08e4bd9998fd2efe5206cbd120a7269cfc5effc
Abbas Ali <Abbas152052@gmail.com>
2022-10-30 22:17:03 +0500

    Firebase JS Added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUqxBZ-d8WyprTcDHqBSZZ5YCQAJQxw0I",
+    authDomain: "fir-a14e2.firebaseapp.com",
+    projectId: "fir-a14e2",
+    storageBucket: "fir-a14e2.appspot.com",
+    messagingSenderId: "625859927315",
+    appId: "1:625859927315:web:4cc63bf3a7794196374ee6",
+    measurementId: "G-SEE3YD72E9"
+  };
